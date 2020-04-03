80ec396cabfac6ffcccf20e48e2fde84dd5caadb
taoqf <tao_qiufeng@126.com>
2020-04-03 10:18:17 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 
-export default function awx14(param1: string) {
+export default function show_action_sheet(itemList: string[], itemColor?: string) {
+	return new Promise<string>((resolve) => {
+		wx.showActionSheet({
+			itemColor,
+			itemList,					// 按钮的文字数组，数组长度最大为6个
+			success(res) {
+				const ret = itemList.find((_v, i) => {
+					return i === res.tapIndex;
+				});
+				resolve(ret);
+			},
+			fail() {
+				resolve('');
+			}
+		});
+	});
 }
